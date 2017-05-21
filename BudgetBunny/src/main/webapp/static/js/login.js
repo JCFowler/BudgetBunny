8488c0664bdd5d786c79c6d8b0b54cc1b0f532cb
John Fowler <krasez@yahoo.com>
2017-05-21 18:59:44 -0400

    Updated Login and Create pages

--- BudgetBunny/src/main/webapp/static/js/login.js
+++ BudgetBunny/src/main/webapp/static/js/login.js
@@ -4,9 +4,12 @@
 
 //onClick="validateLogin();"
 
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
 
 function validateLogin() {
-    var x,y, text;
+    var x,y;
+    var text = '';
 
     // Get the value of the input field with id="numb"
     x = document.getElementById("username").value;
@@ -14,20 +17,53 @@ function validateLogin() {
 
 
     // If x is Not a Number or less than one or greater than 10
-    if (x=="" && y=="") {
+    if (x=="") {
     	document.getElementById('username').style.borderColor = "red";
-    	document.getElementById('password').style.borderColor = "red";
     	document.getElementById('username').style.backgroundColor = "#e2b5b5";
-    	document.getElementById('password').style.backgroundColor = "#e2b5b5";
         text = "Please fill in the blanks";
+        document.getElementById("error-msg").innerHTML = text;
+        $('#error-msg').show();
+    } 
+    if(y=="") {
+    	document.getElementById('password').style.borderColor = "red";
+    	document.getElementById('password').style.backgroundColor = "#e2b5b5";
+    	text = "Please fill in the blanks";
+        document.getElementById("error-msg").innerHTML = text;
+        $('#error-msg').show();
     }
     
-    else{
-    	text = "User not found"
+    if (text === '') {
+    	return true;
+    } else {
+    	return false;
     }
-    document.getElementById("valid").innerHTML = text;
 }
 
 var test = document.getElementById("login");
 
-test.addEventListener("click", validateLogin);
+test.addEventListener("click", (event) => {
+	event.preventDefault();
+	const inputIsValid = validateLogin();
+	
+	if (!inputIsValid) {
+		return;
+	}
+	$('#error-msg').hide();
+	var loginBtn = document.querySelector('#login');
+	loginBtn.disabled = true;
+	loginBtn.innerHTML = "Logging in...";
+	$.ajax({
+		type: 'post',
+		url: '/BudgetBunny/login',
+		data: { username: username.value, password: password.value },
+		success: () => {
+			window.location = '/BudgetBunny/home';
+		},
+		error: () => {
+			$('#error-msg').html('Username or Password was incorrect.');
+			$('#error-msg').show();
+			loginBtn.disabled = false;
+			loginBtn.innerHTML = "Log In";
+		}
+	});
+});
