89b6b8f94b4f42f337de95636387e07a1ded654a
kshah007 <knshah045@gmail.com>
2017-05-21 16:03:35 -0400

    Success message

--- BudgetBunny/src/main/webapp/static/js/jozsef.js
+++ BudgetBunny/src/main/webapp/static/js/jozsef.js
@@ -23,8 +23,32 @@ function ajaxCall(data, destUrl)
 	    data: data,    // multiple data sent using ajax
 	    success: function (html) {
 	    	
-	      //TODO: finish ajax;
-	    	//alert("Ajax Success: \n\t" + html);
+	    	//Get the modal
+	    	var modal = document.getElementById('myModal');
+
+	    	// Get the button that opens the modal
+	    	var btn = document.getElementById("purchase");
+
+	    	// Get the <span> element that closes the modal
+	    	var span = document.getElementsByClassName("close")[0];
+
+	    	// When the user clicks the button, open the modal 
+	    	btn.onclick = function() {
+	    	    modal.style.display = "block";
+	    	}
+
+	    	// When the user clicks on <span> (x), close the modal
+	    	span.onclick = function() {
+	    	    modal.style.display = "none";
+	    	}
+
+	    	// When the user clicks anywhere outside of the modal, close it
+	    	window.onclick = function(event) {
+	    	    if (event.target == modal) {
+	    	        modal.style.display = "none";
+	    	    }
+	    	}
+
 	    }
 	  });
 }
@@ -372,7 +396,7 @@ function displayErrorMessage(validData, data)
 $('#submitSetup').click(function()
 {
 	totalBudget = 0;
-	remainingBudget = 0;
+	remainingBudget = 10;
 	
 	const depData = submitSystematicDeposits(true);
 	const withData = submitSystematicWithdraws(depData);
@@ -474,10 +498,6 @@ function close_div()
 	turnOffHighLight(input);
 	input.val("");
 }
-
-
-
-
 
 
 
