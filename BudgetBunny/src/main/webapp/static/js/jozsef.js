879610f75784daf5b2d2a21a11bd6793879dffc1
kshah007 <knshah045@gmail.com>
2017-05-22 13:11:25 -0400

    Navbar Fixed

--- BudgetBunny/src/main/webapp/static/js/jozsef.js
+++ BudgetBunny/src/main/webapp/static/js/jozsef.js
@@ -559,5 +559,14 @@ function close_div()
 }
 
 
+$(document).ready(function () {
+	  $(".nav li").removeClass("active");//this will remove the active class from  
+	  var pathname = window.location.pathname
+	  switch(pathname ){ 
+	  case('/BudgetBunny/home'): $('#home').addClass('active');break;
+	  case('/BudgetBunny/transaction'): $('#transaction').addClass('active');break;
+	  case('/BudgetBunny/reports'): $('#reports').addClass('active');break;
+	  }
+});
 
 
