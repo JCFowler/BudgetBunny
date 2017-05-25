1238f42f89f818436fc9c2ee2141c0cdb70e6c23
Jozsef Morrissey <jtmorri2@illinois.edu>
2017-05-25 10:55:39 -0400

    Last Minute Project Display Changes

--- BudgetBunny/src/main/java/com/revature/util/GenerateData.java
+++ BudgetBunny/src/main/java/com/revature/util/GenerateData.java
@@ -29,18 +29,29 @@ public class GenerateData {
 	
 	public void generateTransactions(User u)
 	{
-		int max = 50;
+		int max = 100;
 		for(int i = 0; i < max; i++)
 		{
 			Collection<Category> cats = u.getBudget().getCategory();
 			double cost = Math.random() * 500;
 			
 			int year = 2017;
-			int month = (int) (r.nextInt() % 12 + 1);
-			int day = (int) (r.nextInt() % 28 + 1);
+			int month = (int) (r.nextInt() % 12);
+			int day = (int) (r.nextInt() % 28);
+			
+			if(month < 0)
+				month *= -1;
+			if(day < 0)
+				day *= -1;
+			
+			month++;
+			day++;
+			
 			Date date = Date.valueOf(year + "-" + month + "-" + day);
 						
 			int stop = (int) (r.nextInt() % cats.size());
+			if(stop < 0)
+				stop *= -1;
 			int count = 0;
 			
 
